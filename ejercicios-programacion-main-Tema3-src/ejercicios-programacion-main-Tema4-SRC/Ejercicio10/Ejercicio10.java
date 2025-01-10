4239a8ffae0b74777ffa59b1084d98e561a4556a
Jesusrivero0 <jrodriguez1619@ceuandalucia.es>
2025-01-10 20:01:26 +0100

    1 commit

--- ejercicios-programacion-main-Tema3-src/ejercicios-programacion-main-Tema4-SRC/Ejercicio10/Ejercicio10.java
+++ ejercicios-programacion-main-Tema3-src/ejercicios-programacion-main-Tema4-SRC/Ejercicio10/Ejercicio10.java
@@ -1,10 +1,20 @@
 package Ejercicio10;
 
+import Ejercicio9.ArrayUtils;
+
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		String[] array = new String[] {"hola", "adios", "pepe", "Manuel"};
+		
+		String resultado = ArrayUtils.obtenerPalabra(array, 3);
+		
+		System.out.println(resultado);
+		
+		
 	}
 
 }
